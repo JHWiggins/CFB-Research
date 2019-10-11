c22ae63185923a6d99220170874da0e9f12df582
John <56356791+JHWiggins@users.noreply.github.com>
2019-10-11 10:33:29 -0400

    Update GUI_Environment.py

--- GeneralFiles/GUI_Environment.py
+++ GeneralFiles/GUI_Environment.py
@@ -1,1 +1,14 @@
 import pandas as pd
+import PySimpleGUI as sg
+
+def newPlay(self):
+	
+	# In here is where a GUI for an initial input of a new play occuring
+
+def fullGameRundown(self):
+	
+	# In here is where a GUI output for the full game stat rundown
+	
+def fullGamePlayer(self, player):
+	
+	# In here is where a GUI output for the full game stat rundown of a single player
