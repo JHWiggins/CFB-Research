9b9455f71acbedbc8b858a318060cbbb4093c12f
John <56356791+JHWiggins@users.noreply.github.com>
2019-10-14 10:20:27 -0400

    Update GUI_Environment.py

--- GeneralFiles/GUI_Environment.py
+++ GeneralFiles/GUI_Environment.py
@@ -4,7 +4,14 @@ import PySimpleGUI as sg
 def newPlay(self):
 	
 	# In here is where a GUI for an initial input of a new play occuring
-
+	
+	
+	# There are going to be a lot of inputs for new play as this is going to be essentially the data scraper for the entire game data.
+	# Should automatically track the play number, posession number, the number of plays per possesion, and the score at the time of 	  the play
+	# Environment should be dynamic enough for it to change as the inputs are put into the system i.e. what follows a pass play input 	    should be different than a run play input
+	
+	
+	
 def fullGameRundown(self):
 	
 	# In here is where a GUI output for the full game stat rundown
