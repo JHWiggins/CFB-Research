6bf78dc9821fbcbd332f0bcfd3f7eefc138c54cf
John <56356791+JHWiggins@users.noreply.github.com>
2019-10-14 10:58:22 -0400

    Update GUI_Environment.py

--- GeneralFiles/GUI_Environment.py
+++ GeneralFiles/GUI_Environment.py
@@ -9,7 +9,9 @@ def newPlay(self):
 	# There are going to be a lot of inputs for new play as this is going to be essentially the data scraper for the entire game data.
 	# Should automatically track the play number, posession number, the number of plays per possesion, and the score at the time of 	  the play
 	# Environment should be dynamic enough for it to change as the inputs are put into the system i.e. what follows a pass play input 	    should be different than a run play input
-	
+	# This environment should include a mixture of drop down menus and inout boxes
+	# Each should start with the input of the game rosters in order to allow for positions per play to be input quickly
+	# OR THE ROSTER COULD BE NATIVELY SAVED FROM THE START OF THE SEASON WITH PLAYER OBJECTS IN ORDER TO SAVE PLAYER BY PLAYER STATS
 	
 	
 def fullGameRundown(self):
